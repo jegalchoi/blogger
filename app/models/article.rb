a6ac8c9bf03df5d8611f5dc12b6294b8ff0c6b33
jegalchoi <thefreshbegal@gmail.com>
2019-06-13 19:23:35 -0700

    Added some gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :delete_all
   has_many :taggings
   has_many :tags, through: :taggings, dependent: :delete_all
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list
     self.tags.collect do |tag|
